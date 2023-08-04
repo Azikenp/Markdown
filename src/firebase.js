3acb3da899e5d292f3c7881b3987a583152d43df
Azikenp <azikenp26@gmail.com>
2023-08-04 18:04:56 +0100

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore, collection} from '.firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAuhfcjvXxTXmBnYgJ-17uATxjK8nqFVW8",
+  authDomain: "markdown-54e85.firebaseapp.com",
+  projectId: "markdown-54e85",
+  storageBucket: "markdown-54e85.appspot.com",
+  messagingSenderId: "63323601069",
+  appId: "1:63323601069:web:77a3bef902150e54cfee24"
+};
+
+
+const app = initializeApp(firebaseConfig); 
+const db = getFirestore(app);
+
+const notes = collection(db, "notes");
