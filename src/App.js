d5b9bbe79f58c54119550afadbc0247237486453
Azikenp <azikenp26@gmail.com>
2023-08-04 21:46:33 +0100

    adding timestamp to notes app and sorting baseed on  most recent

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ export default function App() {
     const [currentNoteId, setCurrentNoteId] = React.useState("")
 
     const currentNote = notes.find(note => note.id === currentNoteId) || notes[0]
+
+    const sortedNotes = notes.sort((a, b) => b.updatedAt - a.updatedAt)
     
     React.useEffect(() => {
         const unsubscribe =  onSnapshot(notesCollection, function(snapshot){
@@ -36,7 +38,9 @@ export default function App() {
 
     async function createNewNote() {
         const newNote = {
-            body: "# Type your markdown note's title here"
+            body: "# Type your markdown note's title here",
+            createdAt: Date.now(),
+            updatedAt: Date.now()
         }
         const newNoteRef = await addDoc(notesCollection, newNote)
         setCurrentNoteId(newNoteRef.id)
@@ -44,7 +48,9 @@ export default function App() {
     
     async function updateNote(text) {
         const docRef = doc(db, "notes", currentNoteId);
-        await setDoc(docRef, {body: text}, {merge: true})
+        await setDoc(docRef, 
+            {body: text, updatedAt: Date.now()}, 
+            {merge: true})
     }
 
     
@@ -64,7 +70,7 @@ export default function App() {
                 className="split"
             >
                 <Sidebar
-                    notes={notes}
+                    notes={sortedNotes}
                     currentNote={currentNote}
                     setCurrentNoteId={setCurrentNoteId}
                     newNote={createNewNote}
